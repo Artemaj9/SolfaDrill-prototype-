7df17c0338b3c3c6b1b1c1c5ba0de0f0112b9898
Artem <moiseev@physics.msu.ru>
2023-06-03 01:32:14 +0300

    Initial Commit

---
+++ SolfaDrill(prototype)/SolfaDrill_prototype_App.swift
@@ -0,0 +1,17 @@
+//
+//  SolfaDrill_prototype_App.swift
+//  SolfaDrill(prototype)
+//
+//  Created by Artem on 03.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SolfaDrill_prototype_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
