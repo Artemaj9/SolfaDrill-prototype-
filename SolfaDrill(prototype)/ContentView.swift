db5297a0abdf7dcc3d95f73bf74e0e6b2906f17c
Artem <moiseev@physics.msu.ru>
2023-06-03 02:10:22 +0300

    CmajorScale

--- SolfaDrill(prototype)/ContentView.swift
+++ SolfaDrill(prototype)/ContentView.swift
@@ -7,16 +7,105 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 37.0) {
+            Button(action: {
+                //play sound
+                playSound(key: "Tuning")
+            }) {
+                
+                Text("Tunning")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                
+            }
+            Button(action: {
+                //play sound
+                playSound(key: "C")
+            }) {
+                
+                Text("C")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                
+            }
+            Button(action: {
+                //play sound
+                playSound(key: "D")
+            }) {
+                
+                Text("D")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                
+            }
+            Button(action: {
+                //play sound
+                playSound(key: "E")
+            }) {
+                
+                Text("E")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                
+            }
+            Button(action: {
+                //play sound
+                playSound(key: "F")
+            }) {
+                
+                Text("F")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                
+            }
+            Button(action: {
+                //play sound
+                playSound(key: "G")
+            }) {
+                
+                Text("G")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                
+            }
+            Button(action: {
+                //play sound
+                playSound(key: "A")
+            }) {
+                
+                Text("A")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                
+            }
+            Button(action: {
+                //play sound
+                playSound(key: "B")
+            }) {
+                
+                Text("B")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                
+            }
+            Button(action: {
+                //play sound
+                playSound(key: "Chigh")
+            }) {
+                
+                Text("Chigh")
+                    .foregroundColor(.green)
+                    .font(.system(size: 30))
+                
+            }
+            
         }
         .padding()
     }
+ 
 }
 
 struct ContentView_Previews: PreviewProvider {
